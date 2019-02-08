0150f602ca723e6c45cccd51ba8d9b0bfa328ad7
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-08 22:40:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,10 +21,7 @@ end
 
   def self.drop_table
     sql =  <<-SQL
-      DROP TABLE IF NOT EXISTS students (
-        name TEXT,
-        grade INTEGER
-        )
+      DROP TABLE students;
         SQL
     DB[:conn].execute(sql)
   end
