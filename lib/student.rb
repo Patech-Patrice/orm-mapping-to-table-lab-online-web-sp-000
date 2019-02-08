9c5a3f90cf2773373af38cf2e1f4af022dde4f7f
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-08 22:24:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,54 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+
+  attr_accessor :name, :grade
+   
+    def initialize(name, grade)
+      @name = name
+      @grade = grade
+    end
+   
+
+  
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
+
+ 
+
+
+
+
+
+
+
+  def save
+  sql = <<-SQL
+    INSERT INTO songs (name, album) 
+    VALUES (?, ?)
+  SQL
+  DB[:conn].execute(sql, self.name, self.album)
+  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+
+end
+
+
+
+
+  def self.create(name:, album:)
+      song = Song.new(name, album)
+      song.save
+      song
+    end
+
+
   
 end
