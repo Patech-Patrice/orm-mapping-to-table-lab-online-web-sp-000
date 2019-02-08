9c80259d8dc94539c59a3c9cc2f0897728974c42
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-08 22:42:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,11 +29,10 @@ end
 
   def save
     sql = <<-SQL
-    INSERT INTO songs (name, grade)
-    VALUES (?, ?)
-  SQL
-  DB[:conn].execute(sql, self.name, self.grade)
-
+    INSERT INTO students (name, grade)
+    VALUES (?, ?);
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
 end
 
 
