938df644a9af5dd8c162c0674f668d4a105561a0
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-08 22:52:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,16 +3,16 @@ class Student
 attr_accessor :name, :grade
 attr_reader :id
 
-def initialize(name, grade)
+def initialize(name, grade, id=nil)
   @name = name
   @grade = grade
+  @id = id
 end
-
-
 
   def self.create_table
     sql =  <<-SQL
       CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
         name TEXT,
         grade INTEGER
         )
@@ -33,7 +33,8 @@ end
     INSERT INTO students (name, grade)
     VALUES (?, ?);
     SQL
-    DB[:conn].execute(sql, @name, @grade)
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 end
 
   def self.create(name:, grade:)
