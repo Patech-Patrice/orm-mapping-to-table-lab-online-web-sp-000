ddf86d1cbf8f26bef06cf706892e6a5ecd26b6ae
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-08 22:25:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,14 +1,11 @@
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+attr_accessor :name, :grade
 
-  attr_accessor :name, :grade
-
-    def initialize(name, grade)
-      @name = name
-      @grade = grade
-    end
+def initialize(name, grade)
+  @name = name
+  @grade = grade
+end
 
 
 
